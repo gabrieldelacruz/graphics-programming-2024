acb2114e9232d6cf7aa81c29542336192d563ef8
mwpodgorni <mwpodgorni@gmail.com>
2024-02-02 10:40:01 +0100

    Exercise 00.1

--- libraries/itugl/src/ituGL/core/BufferObject.cpp
+++ libraries/itugl/src/ituGL/core/BufferObject.cpp
@@ -3,47 +3,53 @@
 // Create the object initially null, get object handle and generate 1 buffer
 BufferObject::BufferObject() : Object(NullHandle)
 {
-    Handle& handle = GetHandle();
-    // (todo) 00.1: Generate 1 buffer
+	Handle& handle = GetHandle();
+	// (todo) 00.1: Generate 1 buffer
+	glGenBuffers(1, &handle);
 }
 
 // Get object handle and delete 1 buffer
 BufferObject::~BufferObject()
 {
-    Handle& handle = GetHandle();
-    // (todo) 00.1: Delete 1 buffer
+	Handle& handle = GetHandle();
+	// (todo) 00.1: Delete 1 buffer
 }
 
 // Bind the buffer handle to the specific target
 void BufferObject::Bind(Target target) const
 {
-    Handle handle = GetHandle();
-    // (todo) 00.1: Bind buffer
+	Handle handle = GetHandle();
+	// (todo) 00.1: Bind buffer
+	glBindBuffer(target, handle);
 }
 
 // Bind the null handle to the specific target
 void BufferObject::Unbind(Target target)
 {
-    Handle handle = NullHandle;
-    // (todo) 00.1: Bind null buffer
+	Handle handle = NullHandle;
+	// (todo) 00.1: Bind null buffer
 }
 
 // Get buffer Target and allocate buffer data
 void BufferObject::AllocateData(size_t size, Usage usage)
 {
-    Target target = GetTarget();
-    // (todo) 00.1: Allocate without initial data (you can use nullptr instead)
+	Target target = GetTarget();
+	Bind();
+	glBufferData(static_cast<GLenum>(target), size, nullptr, static_cast<GLenum>(usage));
+	// (todo) 00.1: Allocate without initial data (you can use nullptr instead)
 }
 
 // Get buffer Target and allocate buffer data
 void BufferObject::AllocateData(std::span<const std::byte> data, Usage usage)
 {
-    Target target = GetTarget();
-    // (todo) 00.1: Allocate with initial data, specifying the size in bytes and the pointer to the data
+	Target target = GetTarget();
+	Bind();
+	glBufferData(static_cast<GLenum>(target), data.size_bytes(), data.data(), static_cast<GLenum>(usage));
+	// (todo) 00.1: Allocate with initial data, specifying the size in bytes and the pointer to the data
 }
 
 // Get buffer Target and set buffer subdata
 void BufferObject::UpdateData(std::span<const std::byte> data, size_t offset)
 {
-    // (todo) 00.5: Update buffer subdata
+	// (todo) 00.5: Update buffer subdata
 }
--- libraries/itugl/src/ituGL/geometry/VertexArrayObject.cpp
+++ libraries/itugl/src/ituGL/geometry/VertexArrayObject.cpp
@@ -1,48 +1,55 @@
 #include <ituGL/geometry/VertexArrayObject.h>
 
 #include <ituGL/geometry/VertexAttribute.h>
+#include <cstdio>
 
 // Create the object initially null, get object handle and generate 1 vertex array
 VertexArrayObject::VertexArrayObject() : Object(NullHandle)
 {
-    Handle& handle = GetHandle();
-    // (todo) 00.1: Generate 1 vertex array
+	Handle& handle = GetHandle();
+	// (todo) 00.1: Generate 1 vertex array
+	glGenVertexArrays(1, &handle);
 }
 
 // Get object handle and delete 1 vertex array
 VertexArrayObject::~VertexArrayObject()
 {
-    Handle& handle = GetHandle();
-    // (todo) 00.1: Delete 1 vertex array
+	Handle& handle = GetHandle();
+	// (todo) 00.1: Delete 1 vertex array
+	if (handle != 0) {
+		glDeleteVertexArrays(1, &handle);
+		handle = 0;
+	}
+	printf("HELLO\n");
 }
 
 // Bind the vertex array handle to the specific target
 void VertexArrayObject::Bind() const
 {
-    Handle handle = GetHandle();
-    // (todo) 00.1: Bind vertex array
+	Handle handle = GetHandle();
+	// (todo) 00.1: Bind vertex array
+	glBindVertexArray(handle);
 }
 
 // Bind the null handle to the specific target
 void VertexArrayObject::Unbind()
 {
-    Handle handle = NullHandle;
-    // (todo) 00.1: Bind null vertex array
+	Handle handle = NullHandle;
+	// (todo) 00.1: Bind null vertex array
+	glBindVertexArray(0);
 }
 
 // Sets the VertexAttribute pointer and enables the VertexAttribute in that location
 void VertexArrayObject::SetAttribute(GLuint location, const VertexAttribute& attribute, GLint offset, GLsizei stride)
 {
-    // Get the attribute properties in OpenGL expected format
-    GLint components = attribute.GetComponents();
-    GLenum type = static_cast<GLenum>(attribute.GetType());
-    GLboolean normalized = attribute.IsNormalized() ? GL_TRUE : GL_FALSE;
+	// Get the attribute properties in OpenGL expected format
+	GLint components = attribute.GetComponents();
+	GLenum type = static_cast<GLenum>(attribute.GetType());
+	GLboolean normalized = attribute.IsNormalized() ? GL_TRUE : GL_FALSE;
 
-    // Compute the attribute pointer
-    const unsigned char* pointer = nullptr; // Actual base pointer is in VBO
-    pointer += offset;
+	// Compute the attribute pointer
+	const unsigned char* pointer = nullptr; // Actual base pointer is in VBO
+	glVertexAttribPointer(location, components, type, normalized, stride, pointer);
 
-    // (todo) 00.1: Set the VertexAttribute pointer in that location
-
-    // (todo) 00.1: Finally, we enable the VertexAttribute in that location
+	glEnableVertexAttribArray(location);
 }
