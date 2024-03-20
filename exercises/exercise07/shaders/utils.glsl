58615de6ac391066e02b10fbe239b8e5464730ad
Gabriel de la Cruz <syxtem@yahoo.com>
2024-03-20 20:22:56 +0100

    Exercise 07

---
+++ exercises/exercise07/shaders/utils.glsl
@@ -0,0 +1,41 @@
+
+// Returns camera position, extracted from view matrix
+vec3 GetCameraPosition(mat4 viewMatrix)
+{
+	vec3 position = viewMatrix[3].xyz;
+	position = -(transpose(viewMatrix) * vec4(position, 0)).xyz;
+	return position;
+}
+
+// Gets vector halfway between two other vectors (typically, light and view)
+vec3 GetHalfVector(vec3 v1, vec3 v2)
+{
+   return normalize(v1 + v2);
+}
+
+// Gets the direction from a point to another
+vec3 GetDirection(vec3 fromPosition, vec3 toPosition)
+{
+	return normalize(toPosition - fromPosition);
+}
+
+// Returns the dot product, clamped only to positive values
+float ClampedDot(vec3 v1, vec3 v2)
+{
+	return max(dot(v1, v2), 0);
+}
+
+// Constructs the 3D normal using only XY values (computing implicit Z)
+vec3 GetImplicitNormal(vec2 normal)
+{
+	// (todo) 07.3: Obtain the implicit Z component of the normal
+	return vec3(0);
+}
+
+// Obtains a position in view space using the depth buffer and the inverse projection matrix
+vec3 ReconstructViewPosition(sampler2D depthTexture, vec2 texCoord, mat4 invProjMatrix)
+{
+	// (todo) 07.4: Reconstruct the position, using the screen texture coordinates and the depth
+	return vec3(0);
+}
+
---
+++ libraries/itugl/src/ituGL/renderer/ForwardRenderPass.cpp
@@ -0,0 +1,48 @@
+#include <ituGL/renderer/ForwardRenderPass.h>
+
+#include <ituGL/camera/Camera.h>
+#include <ituGL/shader/Material.h>
+#include <ituGL/geometry/VertexArrayObject.h>
+#include <ituGL/renderer/Renderer.h>
+
+ForwardRenderPass::ForwardRenderPass()
+    : ForwardRenderPass(0)
+{
+}
+
+ForwardRenderPass::ForwardRenderPass(int drawcallCollectionIndex)
+    : m_drawcallCollectionIndex(drawcallCollectionIndex)
+{
+}
+
+void ForwardRenderPass::Render()
+{
+    Renderer& renderer = GetRenderer();
+
+    const Camera& camera = renderer.GetCurrentCamera();
+    const auto& lights = renderer.GetLights();
+    const auto& drawcallCollection = renderer.GetDrawcalls(m_drawcallCollectionIndex);
+
+    // for all drawcalls
+    for (const Renderer::DrawcallInfo& drawcallInfo : drawcallCollection)
+    {
+        // (todo) 07.0: Prepare drawcall states
+        renderer.PrepareDrawcall(drawcallInfo);
+
+        std::shared_ptr<const ShaderProgram> shaderProgram = drawcallInfo.material.GetShaderProgram();
+
+        //for all lights
+        bool first = true;
+        unsigned int lightIndex = 0;
+        while (renderer.UpdateLights(shaderProgram, lights, lightIndex))
+        {
+            // (todo) 07.0: Set the renderstates
+            renderer.SetLightingRenderStates(first);
+
+            // (todo) 07.0: Draw
+            drawcallInfo.drawcall.Draw();
+
+            first = false;
+        }
+    }
+}
