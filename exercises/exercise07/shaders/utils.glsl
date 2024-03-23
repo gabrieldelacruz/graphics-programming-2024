9797a80fec719b82cf69eb9831f7b6a77a182502
Gabriel de la Cruz <syxtem@yahoo.com>
2024-03-23 20:44:42 +0100

    Exercise 07 solutions

--- exercises/exercise07/shaders/utils.glsl
+++ exercises/exercise07/shaders/utils.glsl
@@ -28,14 +28,17 @@ float ClampedDot(vec3 v1, vec3 v2)
 // Constructs the 3D normal using only XY values (computing implicit Z)
 vec3 GetImplicitNormal(vec2 normal)
 {
-	// (todo) 07.3: Obtain the implicit Z component of the normal
-	return vec3(0);
+	float z = sqrt(1.0f - normal.x * normal.x - normal.y * normal.y);
+	return vec3(normal, z);
 }
 
 // Obtains a position in view space using the depth buffer and the inverse projection matrix
 vec3 ReconstructViewPosition(sampler2D depthTexture, vec2 texCoord, mat4 invProjMatrix)
 {
-	// (todo) 07.4: Reconstruct the position, using the screen texture coordinates and the depth
-	return vec3(0);
+	// Reconstruct the position, using the screen texture coordinates and the depth
+	float depth = texture(depthTexture, texCoord).r;
+	vec3 clipPosition = vec3(texCoord, depth) * 2.0f - vec3(1.0f);
+	vec4 viewPosition = invProjMatrix * vec4(clipPosition, 1.0f);
+	return viewPosition.xyz / viewPosition.w;
 }
 
